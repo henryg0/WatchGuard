9e1d193b0c5970a03a89bb4016c6e3ec4df73fc4
henryg0 <hgip@uci.edu>
2021-11-03 13:17:13 -0700

    added GUI functionality

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 package src;
 import src.HeartRateSensor;
+import javax.swing.JFrame;
 
 class App {
     public static void main() {
@@ -7,5 +8,16 @@ class App {
         // assign ports to each sensor
         int heartRateSensorPort = 0;
         HeartRateSensor heartRateSensor = new HeartRateSensor(heartRateSensorPort);
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                startGUI();
+            }
+        });
+    }
+    private static void startGUI() {
+        JFrame jf = new JFrame("WatchGuard");
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.pack();
+        jf.setVisible(true);
     }
 }
